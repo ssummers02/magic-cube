38d563098d57d92605ec783086ffd81bb3f0601d
Kim Nikita <51363591+ssummers02@users.noreply.github.com>
2021-02-17 17:38:16 +0300

    Add files via upload

---
+++ Magic cube/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+
+namespace Magic_cube
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkmagickButton = new System.Windows.Forms.Button();
+            this.createMagickButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            // 
+            // checkmagickButton
+            // 
+            this.checkmagickButton.Location = new System.Drawing.Point(571, 418);
+            this.checkmagickButton.Name = "checkmagickButton";
+            this.checkmagickButton.Size = new System.Drawing.Size(109, 27);
+            this.checkmagickButton.Text = "Проверить матрицу";
+            this.checkmagickButton.Click += new System.EventHandler(this.CheckmagickButtonClick);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(571+109, 418);
+            this.clearButton.Name = "checkmagickButton";
+            this.clearButton.Size = new System.Drawing.Size(109, 27);
+            this.clearButton.Text = "Очистить";
+            this.clearButton.Click += new System.EventHandler(this.CleanButtonClick);
+            // 
+            // createMagickButton
+            // 
+            this.createMagickButton.Location = new System.Drawing.Point(571, 447);
+            this.createMagickButton.Name = "createMagickButton";
+            this.createMagickButton.Size = new System.Drawing.Size(217, 27);
+            this.createMagickButton.Text = "Создать магический квадрат";
+            this.createMagickButton.Click += new System.EventHandler(this.CreateMagickButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Magic_cube.Properties.Resources.color_2174049_1280;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.createMagickButton);
+            this.Controls.Add(this.checkmagickButton);
+            this.Controls.Add(this.clearButton);
+
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Cube";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button createMagickButton;
+        private System.Windows.Forms.Button checkmagickButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label magicLabel;
+        #endregion
+    }
+}
---
+++ Magic cube/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Magic_cube
+{
+    public partial class Form1 : Form
+    {
+        private int N = 3;
+        List<List<int>> list = new List<List<int>>();
+        List<List<TextBox>> boxList = new List<List<TextBox>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateTextBoxes();
+        }
+
+        private void PrintMagicLabel(int a)
+        {
+            magicLabel = new Label();
+            magicLabel.AutoSize = true;
+            magicLabel.BackColor = Color.Transparent;
+            magicLabel.Font = new Font("Times New Roman", 14F, FontStyle.Bold);
+            magicLabel.ForeColor = Color.Gray;
+            magicLabel.Location = new Point(20, 20 + 35 * N);
+            magicLabel.Name = "magicLabel";
+            magicLabel.Size = new Size(200, 19);
+            magicLabel.Text = $@"Не магический квадрат";
+
+            if (a == 1)
+                magicLabel.Text = $@"Магический квадрат";
+
+            this.Controls.Add(magicLabel);
+        }
+
+        private void DeleteControls()
+        {
+            this.Controls.Remove(magicLabel);
+        }
+
+        private void ClearLists()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    list.Clear();
+                }
+            }
+        }
+
+        private void CreateTextBoxes()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                boxList.Add(new List<TextBox>());
+
+                for (int j = 0; j < N; j++)
+                {
+                    boxList.Add(new List<TextBox>());
+                    var tempTextBox = new TextBox()
+                    {
+                        Name = Convert.ToString(i) + Convert.ToString(j),
+                        Location = new Point(20 + 35 * j, 20 + 35 * i),
+                        Size = new Size(30, 30),
+                        Text = @"0"
+                    };
+                    boxList[i].Add(tempTextBox);
+                    Controls.Add(boxList[i][j]);
+                }
+            }
+        }
+
+        private int ListChecker()
+        {
+            DeleteControls();
+
+            for (int i = 0; i < N; i++)
+            {
+                list.Add(new List<int>());
+
+                for (int j = 0; j < N; j++)
+                {
+                    boxList[i][j].Text = boxList[i][j].Text;
+                    bool success = Int32.TryParse(boxList[i][j].Text, out var number);
+
+                    if (success)
+                        list[i].Add(number);
+                    else
+                    {
+                        MessageBox.Show($@"Проверьте значения!");
+                        return -1;
+                    }
+                }
+            }
+
+            return 1;
+        }
+
+        private int Parse()
+        {
+            List<int> valid = new List<int>()
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            };
+            List<int> ch = new List<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                int s = 0;
+
+                for (int j = 0; j < N; j++)
+                {
+                    s += list[i][j];
+                    ch.Add(list[i][j]);
+                }
+
+                if (s != 15)
+                    return -1;
+            }
+
+            ch.Sort();
+
+            for (int i = 0; i < valid.Count; i++)
+            {
+                if (valid[i] != ch[i])
+                {
+                    MessageBox.Show(Convert.ToString(ch));
+                    return -1;
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                int s = 0;
+
+                for (int j = 0; j < N; j++)
+                    s += list[j][i];
+
+                if (s != 15)
+                    return -1;
+            }
+
+            int a = 0;
+            for (int i = 0; i < N; i++)
+                a += list[i][i];
+            if (a != 15)
+                return -1;
+
+            int k = N - 1;
+            a = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                a += list[i][k];
+                k--;
+            }
+
+            if (a != 15)
+                return -1;
+
+            return 1;
+        }
+
+        private void CleanButtonClick(object sender, EventArgs e)
+        {
+            ClearLists();
+            DeleteControls();
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    boxList[i][j].Text = @"0";
+                }
+            }
+        }
+
+        private void CheckmagickButtonClick(object sender, EventArgs e)
+        {
+            if (ListChecker() == 1)
+            {
+                PrintMagicLabel(Parse());
+                ClearLists();
+            }
+            else
+            {
+                ClearLists();
+                DeleteControls();
+            }
+        }
+
+        private void CreateMagickButtonClick(object sender, EventArgs e)
+        {
+            DeleteControls();
+            boxList[0][0].Text = @"2";
+            boxList[0][1].Text = @"7";
+            boxList[0][2].Text = @"6";
+
+            boxList[1][0].Text = @"9";
+            boxList[1][1].Text = @"5";
+            boxList[1][2].Text = @"1";
+
+            boxList[2][0].Text = @"4";
+            boxList[2][1].Text = @"3";
+            boxList[2][2].Text = @"8";
+
+            CheckmagickButtonClick(sender, e);
+        }
+    }
+}
